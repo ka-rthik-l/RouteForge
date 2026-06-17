8486acddad88bed47b5d7eff29d85e690e0d9fc6
ka-rthik-l <karthiklathesh20@gmail.com>
2026-06-17 22:46:55 +0530

    Fix server entry export for Vercel

--- src/server.ts
+++ src/server.ts
@@ -37,8 +37,7 @@ async function normalizeCatastrophicSsrResponse(response: Response): Promise<Res
   });
 }
 
-export default {
-  async fetch(request: Request, env: unknown, ctx: unknown) {
+export default async function (request: Request, env: unknown, ctx: unknown) {
     try {
       const handler = await getServerEntry();
       const response = await handler.fetch(request, env, ctx);
@@ -50,5 +49,4 @@ export default {
         headers: { "content-type": "text/html; charset=utf-8" },
       });
     }
-  },
-};
+}
